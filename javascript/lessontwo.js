feac48902501d5dfe33700e508b591539a540ea5
samim.azimi <samimelite8@gmail.com>
2022-09-10 22:22:04 +0430

    lesson two

---
+++ javascript/lessontwo.js
@@ -0,0 +1,87 @@
+console.log("Session about Object")
+
+// function sayHi() {
+//     console.log('Hi ' + this.name)
+// }
+// let user = {
+//     name: "Nazir",
+//     age: 43,
+//     sayHi: sayHi
+// };
+
+// let person = new Object();
+// person.name = "kabir";
+// person.age = 23;
+// person.job = "Developer"
+// person.sayHi = sayHi;
+
+// user.sayHi();
+// person.sayHi();
+
+// console.log("job" in user)
+
+// console.log(user["age"])
+
+// console.log("Type of our Value is " + typeof (person))
+
+
+// for (let pro in person) {
+//     console.log(person[pro])
+// }
+
+// console.log(person["name"], person["age"], person["job"])
+
+
+// 3 user, 
+// let id = Symbol('id')
+// let id2 = Symbol('id')
+
+
+// function User(id, name, age, job, isAdmin) {
+//     this.id = id
+//     this.name = name,
+//         this.job = job,
+//         this.age = age,
+//         this.isAdmin = isAdmin
+
+//     this.sayyourname = function () {
+//         console.log(`hi my name is ${this.name}`)
+//     }
+// }
+// let user1 = new User(id, "ahmad", 23, "developer", true)
+// let user2 = new User(id2, "kabir", 23, "Network Manager", false)
+
+// user1.sayyourname();
+// user2.sayyourname();
+
+// console.log(user1.id === user2.id2)
+
+
+// console.log(typeof (user2))
+
+// let arr = [
+//     ["ahmad", "kabir", "Mahmood", "person"], 
+//     ["ahmad", "kabir", "Mahmood", "person"],
+//     ["ahmad", "kabir", "Mahmood", "person"]
+// ]
+let person = ["ahmad", "kabir", "Mahmood", "ahmad", "ahmad"]
+// console.log(person[0])
+// console.log(person[1])
+// person.push('new item')
+// person.pop();
+// person.shift()
+// console.log(person.includes("kabir"))
+// console.log(person.lastIndexOf("ahmad"))
+// console.log(person.indexOf("kabir"))
+// console.log(person.length)
+// let i = 0;
+// while (i < person.length) {
+//     console.log(person[i])
+//     i++
+// }
+// for (let i = 0; i < person.length; i++) {
+
+//     console.log(person[i])
+// }
+
+// console.log(typeof person)
